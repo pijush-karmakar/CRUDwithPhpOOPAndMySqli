d04ce4064a00d0cfda6c609e7ca4e4e4638d03f4
pijush karmakar <pijushkarmakar45@gmail.com>
2017-07-16 17:55:31 +0600

    Dynamic CRUD with PHP OOP & MySqli

---
+++ CRUD_with_PHP_OOP&Mysqli/inc/header.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CRUD With MySqli</title>
+
+	<link rel="stylesheet" href="inc/bootstrap.min.css">
+
+</head>
+
+<body>
+
+<div class="container">
+	<div class="well text-center">
+		<h3> Dynamic CRUD with PHP OOP & MySqli </h3>
+	</div>
+
+   
